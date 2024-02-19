bbc5401eb62ba527b806131d71fe2bc15d56c043
Mitchell <mitchell@tuxbox.lan>
2024-02-18 16:02:08 -0800

    Version 1 -- needs Docs

--- src/GranularLammpsDump.jl
+++ src/GranularLammpsDump.jl
@@ -1,10 +1,10 @@
-# // This module offers several tools for making LAMMPS dump files
-# // more approachable.  
-module LAMMPStools
+# // This module offers several tools for making LAMMPS dump files more approachable.
+# // Also a couple of ways to make movies.  
+module GranularLammpsDump
 
-export readdump, getNatoms, parsestep, dump2mat, dataloader, makemovie_type1, makemovie_type2
+export getNatoms, reaaddump, parsestep, dump2mat, settingsloader, setdefaults, menu, makemovie_allgrains, makemovie_xslice
 
-using DelimitedFiles, Plots, Colors
+using DelimitedFiles, Plots, StatsBase, Colors
 
 # // Get the Natoms in the simulation
 function getNatoms(inputfile)
@@ -22,7 +22,7 @@ function readdump(inputfile)
     rawdump = readdlm(inputfile);
     Natoms = Int(rawdump[4,1]);
     ind = findall(a->!isempty(a), rawdump[:,8]);
-    rawdump = rawdump[ind,1:11];
+    rawdump = rawdump[ind,1:end-2];
     ind = findall(a->a!="ITEM:",rawdump[:,1]);
     rawdump = rawdump[ind,:];
     
@@ -99,19 +99,56 @@ function dump2mat(stepdict, exportflag)
     return mat
 end
 
-# // simple dataloader before visualizing
-function dataloader(inputfile)
-# // read in the bashed dump file
-# // dump is read in like this:
-# | 1  |  2    | 3 | 4 | 5 | 6  | 7  | 8  | 9  | 10 | 11 | time
-# | ID | GROUP | x | y | z | vx | vy | vz | ux | uy | uz | time
-    dump = readdump(inputfile);
-    return dump
+# // Above -- dump handling
+# // Below -- visualizing
+
+# // Load the settings.conf file into a settings dictionary.
+function settingsloader()
+   settings = Dict();
+   settingsmatrix = readdlm("settings.conf");
+   for it in 1:length(settingsmatrix[:,1])
+       nextentry = Dict(settingsmatrix[it,1] => settingsmatrix[it,2]);
+       merge!(settings,nextentry);
+   end
+   return settings
+end
+
+# // Brings up the settings menu in system program nano.
+function menu()
+    run(`nano settings.conf`)
+end
+
+# // Create the first settings file for the project.
+function setdefaults()
+    defaults = ["grainsize" 25;
+                "bordersize" 2;
+                "opacity" 0.5;
+                "radialview" 10;
+                "azimuthalview" 5;
+                "labelsfontsize" 24;
+                "color1" "blue";
+                "color2" "green";
+                "color3" "yellow";
+                "color4" "orange";
+                "color5" "red";
+                "fps" 20;
+                "Tlower" 0.3;
+                "Tlow" 0.75;
+                "Thigh" 1.25;
+                "Thigher" 3]
+    file = open("settings.conf", "w");
+    writedlm(file, defaults, ' '); # // delimiter is a space
+    close(file);
 end
 
 # // Make an mp4 movie with 5 groups color coded by initial x position
-function makemovie_type1(dump,skips)
-    # // Call other libraries to help visualizeext()
+function makemovie_allgrains(dump,skips,dumpfile="allgrains")
+    if !isfile("settings.conf")
+        display("+_+_+_+_+_+_+_+_+_+_+_+_+_+_+")
+        display("Run setdefaults() and try again!")
+        display("+_+_+_+_+_+_+_+_+_+_+_+_+_+_+")
+    end
+    settings = settingsloader();
     gr(size=(1200,1200)); # // define the output size
     default(legend = false) # // turn off legend in the movie
     init = get(dump,0,3);
@@ -122,10 +159,10 @@ function makemovie_type1(dump,skips)
     c5 = findall(a->a>=3 && a <= 5,init[:,3]);
 
     # vizualizating settings
-    grainsize = 25;
-    bordersize = 2;
-    opacity = 0.50;
-    camera_angle = (10,5);
+    grainsize = get(settings, "grainsize", 25);
+    bordersize = get(settings, "bordersize", 2);
+    opacity = get(settings, "opacity", 0.5);
+    camera_angle = (get(settings, "radialview", 10), get(settings,"aziumuthalview", 5));
 
     # // make the plots that form the movie
     anim = @animate for ts in 0:skips:length(dump) - 1
@@ -138,12 +175,12 @@ function makemovie_type1(dump,skips)
             ylims = (-5,5),
             ylabel = "y",
             zlabel = "z",
-            guidefont=font(24),
+            guidefont=font(get(settings,"labelsfontsize", 24)),
             zlims = (0,14),
             camera = camera_angle, # // measured in degrees
             aspect_ratio = :equal,
             msize = grainsize, 
-            mcolor = "purple",
+            mcolor = get(settings,"color1", "blue"),
             malpha = opacity,
             mscolor = "black",
             mswidth = bordersize
@@ -151,7 +188,7 @@ function makemovie_type1(dump,skips)
         scatter!(
             steps[c2,3],steps[c2,4],steps[c2,5], 
             msize = grainsize, 
-            mcolor = "red",
+            mcolor = get(settings,"color2", "green"),
             malpha = opacity,
             mscolor = "black",
             mswidth = bordersize
@@ -159,7 +196,7 @@ function makemovie_type1(dump,skips)
         scatter!(
             steps[c3,3],steps[c3,4],steps[c3,5], 
             msize = grainsize, 
-            mcolor = "orange",
+            mcolor = get(settings,"color3", "yellow"),
             malpha = opacity,
             mscolor = "black",
             mswidth = bordersize
@@ -167,7 +204,7 @@ function makemovie_type1(dump,skips)
         scatter!(
             steps[c4,3],steps[c4,4],steps[c4,5], 
             msize = grainsize, 
-            mcolor = "green",
+            mcolor = get(settings,"color4", "orange"),
             malpha = opacity,
             mscolor = "black",
             mswidth = bordersize
@@ -175,39 +212,48 @@ function makemovie_type1(dump,skips)
         scatter!(
             steps[c5,3],steps[c5,4],steps[c5,5], 
             msize = grainsize, 
-            mcolor = "blue",
+            mcolor = get(settings,"color5", "red"),
             malpha = opacity,
             mscolor = "black",
             mswidth = bordersize
             )
     end
-    mp4(anim, fps=20, loop=0, verbose=false, show_msg=true)
+    mp4(anim, fps=get(settings,"fps",20) , loop=0, verbose=false, show_msg=true);
+    return movie
 end
 
-# // Make an mp4 movie with 5 groups color coded by current vy
-function makemovie_type2(dump,skips)
-    # // Call other libraries to help visualizeext()
+# // Make an mp4 movie with 5 groups color coded by current vy 
+function makemovie_xslice(dump,skips,dumpfile="xslice")
+     if !isfile("settings.conf")
+        display("+_+_+_+_+_+_+_+_+_+_+_+_+_+_+")
+        display("Run setdefaults() and try again!")
+        display("+_+_+_+_+_+_+_+_+_+_+_+_+_+_+")
+    end
+    settings = settingsloader();
     gr(size=(1200,1200)); # // define the output size
     default(legend = false) # // turn off legend in the movie
     init = get(dump,0,3);
     sample = findall(a->a>=-5 && a < -3,init[:,3]);
     
     # vizualizating settings
-    grainsize = 25;
-    bordersize = 2;
-    opacity = 0.50;
-    camera_angle = (10,5);
+    grainsize = get(settings, "grainsize", 25);
+    bordersize = get(settings, "bordersize", 2);
+    opacity = get(settings, "opacity", 0.5);
+    camera_angle = (get(settings, "radialview", 10),get(settings,"aziumuthalview", 5));
 
     # // make the plots that form the movie
     anim = @animate for ts in 0:skips:length(dump) - 1
         steps = get(dump,ts,3);
         steps = steps[sample,:];
-        maxvy = maximum(abs.(steps[:,7]));
-        c1 = findall(a->a>=-maxvy && a < -maxvy/2, steps[:,7]);
-        c2 = findall(a->a>=-maxvy/2 && a < -maxvy/10, steps[:,7]);
-        c3 = findall(a->a>=-maxvy/10 && a < maxvy/10, steps[:,7]);
-        c4 = findall(a->a>= maxvy/10 && a < maxvy/2, steps[:,7]);
-        c5 = findall(a->a>= maxvy/2 && a <= maxvy, steps[:,7]);
+        meanvy = mean(steps[:,7]);
+        Ty = (steps[:,7] .- meanvy).^2;
+        maxTy = maximum(Ty);
+        meanTy = mean(Ty);
+        c1 = findall(a->a>=0 && a < get(settings,"Tlower", 0.3)*meanTy, Ty);
+        c2 = findall(a->a>=get(settings,"Tlower", 0.3)*meanTy && a < get(settings,"Tlow", 0.75)*meanTy, Ty);
+        c3 = findall(a->a>=get(settings,"Tlow", 0.75)*meanTy && a < get(settings,"Thigh", 1.25)*meanTy, Ty);
+        c4 = findall(a->a>= get(settings,"Thigh", 1.25)*meanTy && a < get(settings,"Thigher", 3)*meanTy, Ty);
+        c5 = findall(a->a>= get(settings,"Thigher", 3)*meanTy && a <= maxTy, Ty);
         # // add something here about sorting into a color by x position
         scatter(
             steps[c1,3],steps[c1,4],steps[c1,5], 
@@ -216,12 +262,12 @@ function makemovie_type2(dump,skips)
             ylims = (-5,5),
             ylabel = "y",
             zlabel = "z",
-            guidefont=font(24),
+            guidefont=font(get(settings,"labelsfontsize", 24)),
             zlims = (0,14),
             camera = camera_angle, # // measured in degrees
             aspect_ratio = :equal,
             msize = grainsize, 
-            mcolor = "red",
+            mcolor = get(settings,"color1", "blue"),
             malpha = opacity,
             mscolor = "black",
             mswidth = bordersize
@@ -229,7 +275,7 @@ function makemovie_type2(dump,skips)
         scatter!(
             steps[c2,3],steps[c2,4],steps[c2,5], 
             msize = grainsize, 
-            mcolor = "purple",
+            mcolor = get(settings,"color2", "green"),
             malpha = opacity,
             mscolor = "black",
             mswidth = bordersize
@@ -237,7 +283,7 @@ function makemovie_type2(dump,skips)
         scatter!(
             steps[c3,3],steps[c3,4],steps[c3,5], 
             msize = grainsize, 
-            mcolor = "blue",
+            mcolor = get(settings,"color3", "yellow"),
             malpha = opacity,
             mscolor = "black",
             mswidth = bordersize
@@ -245,7 +291,7 @@ function makemovie_type2(dump,skips)
         scatter!(
             steps[c4,3],steps[c4,4],steps[c4,5], 
             msize = grainsize, 
-            mcolor = "green",
+            mcolor = get(settings,"color4", "orange"),
             malpha = opacity,
             mscolor = "black",
             mswidth = bordersize
@@ -253,16 +299,14 @@ function makemovie_type2(dump,skips)
         scatter!(
             steps[c5,3],steps[c5,4],steps[c5,5], 
             msize = grainsize, 
-            mcolor = "yellow",
+            mcolor = get(settings,"color5", "red"),
             malpha = opacity,
             mscolor = "black",
             mswidth = bordersize
             )
     end
-    mp4(anim, fps=20, loop=0, verbose=false, show_msg=true)
-end
-
-end # module LAMMPStools
+    mp4(anim, string(dumpfile,".mp4"), fps=get(settings,"fps",20) , loop=0, verbose=false, show_msg=true)
+end #module LAMMPStools
 
 
 # // OLD STUFF DOWN HERE
